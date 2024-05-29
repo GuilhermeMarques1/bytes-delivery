438adc245f8cadc035286f322f944fd54fe3d6f9
GuilhermeMarques1 <g-marques@outlook.com>
2024-05-29 10:29:05 -0300

    feat: Config app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from './styles/theme'
 
-import { Header } from './components/Header'
+import { AppRoutes } from './routes'
 
 export function App() {
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <Header />
+      <AppRoutes />
     </ChakraProvider>
   )
 }
